f37afa05831d8eed9a2fb24b5659c5afaa00cb46
DESKTOP-VTMT4L1\KelixMe <kelixme>
2026-03-29 06:09:58 +0300

    new ui

---
+++ price.js
@@ -0,0 +1,44 @@
+pricelist = [
+    {
+        item_discount: 0,
+        item_price: '70 Руб',
+        item_term: '2 нед. (14д)',
+        pay_link: 'https://pay.cloudtips.ru/p/d97e09bd',
+    },
+    {
+        item_discount: 0,
+        item_price: '130 Руб',
+        item_term: '1 мес. (30д)',
+        pay_link: 'https://pay.cloudtips.ru/p/5805d96d',
+    },
+    {
+        item_discount: 0,
+        item_price: '170 Руб',
+        item_term: '3 мес. (90д)',
+        pay_link: 'https://pay.cloudtips.ru/p/039e249a',
+    },
+    {
+        item_discount: 0,
+        item_price: '250 Руб',
+        item_term: '6 мес. (180д)',
+        pay_link: 'https://pay.cloudtips.ru/p/1a5bda74',
+    },
+    {
+        item_discount: 30,
+        item_price: '500 Руб',
+        item_term: '1 год. (365д)',
+        pay_link: 'https://pay.cloudtips.ru/p/034c84f9',
+    },
+    {   
+        item_discount: 63,
+        item_price: '2000 Руб',
+        item_term: 'Навсегда',
+        pay_link: 'https://pay.cloudtips.ru/p/8c833222',
+    },
+    {   
+        item_discount: 0,
+        item_price: 'Пожертвование',
+        item_term: 'На развитие проекта',
+        pay_link: 'https://pay.cloudtips.ru/p/c6fe8558',
+    },
+]
