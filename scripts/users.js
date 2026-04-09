dc8bdf8a6cb994c5603ba1c1b3561df9df0babc9
DESKTOP-VTMT4L1\KelixMe <kelixme>
2026-04-09 13:19:24 +0300

    8888

--- scripts/users.js
+++ scripts/users.js
@@ -329,7 +329,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const results = [];
         
         // Разбиваем на маленькие пачки по 10 ID для более быстрой обработки
-        const chunkSize = 10;
+        const chunkSize = 1;
         for (let i = 0; i < uniqueIds.length; i += chunkSize) {
             const chunk = uniqueIds.slice(i, i + chunkSize);
             
