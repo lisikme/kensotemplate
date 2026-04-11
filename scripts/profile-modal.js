f6ebea106660e735ea8fb9ec9bfe7e63c005dd33
DESKTOP-VTMT4L1\KelixMe <kelixme>
2026-04-11 21:18:53 +0300

    87777

--- scripts/profile-modal.js
+++ scripts/profile-modal.js
@@ -230,10 +230,6 @@
             <div class="profile-modal-container">
                 <div class="profile-modal-header">
                     <div class="profile-modal-title">
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
-                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
-                            <circle cx="12" cy="7" r="4"/>
-                        </svg>
                         Профиль пользователя
                     </div>
                     <button class="profile-modal-close" id="profileModalCloseLoader">
